6a5665c97da3b5f0577ff4088a26038dc517a877
rodriguezavellan <rodriguezavellan@gmail.com>
2025-04-16 16:18:23 +0200

    actualizacion url csv

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ seccion = st.sidebar.radio("Ir a:", ["🧹 Limpieza de Datos", "📦 Análisis d
 # Cargar datos
 @st.cache_data
 def cargar_datos():
-    #return pd.read_csv("cafe_limpio.csv", parse_dates=["Transaction Date"])
-    return pd.read_csv("https://github.com/rodriguezavellan/cafeapp/blob/main/cafe_limpio.csv", parse_dates=["Transaction Date"])
+    return pd.read_csv("cafe_limpio.csv", parse_dates=["Transaction Date"])
+    #return pd.read_csv("https://github.com/rodriguezavellan/cafeapp/blob/main/cafe_limpio.csv", parse_dates=["Transaction Date"])
     
 
 cafe = cargar_datos()
