2e1c72f6442b6f2b1e434880e101ae6085655640
rodriguezavellan <rodriguezavellan@gmail.com>
2025-04-17 10:33:11 +0200

    correccion errores en graficos2

--- app.py
+++ app.py
@@ -379,27 +379,10 @@ elif seccion == "💰 Ventas y Patrones":
         # Ingreso total por tipo
         import plotly.express as px
         st.subheader("Ingreso total: Comida vs Bebida")
-
-        # Eliminar valores nulos
-        cafe_filtrado = cafe.dropna(subset=["Tipo", "Ingreso"])
+        tipo_resumen = cafe.groupby("Tipo")["Ingreso"].sum().reset_index()
+        tipo_resumen
         
-        # Convertir ingreso a numérico
-        cafe_filtrado["Ingreso"] = pd.to_numeric(cafe_filtrado["Ingreso"], errors="coerce")
-        
-        # Agrupar ingresos
-        tipo_resumen = cafe_filtrado.groupby("Tipo")["Ingreso"].sum().reset_index()
-        
-        # Graficar
-        fig7 = px.bar(
-            tipo_resumen,
-            x="Tipo",
-            y="Ingreso",
-            color="Tipo",
-            title="Ingreso total: Comida vs Bebida",
-            color_discrete_sequence=px.colors.qualitative.Set2
-        )
-        
-        st.plotly_chart(fig7)
+      
         
     col1, col2 = st.columns(2)
 
